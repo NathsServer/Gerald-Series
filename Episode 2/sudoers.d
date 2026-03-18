39b235b5df15f5435619ad84099804fb5a85eb04
NathsServer <nathanbardenit@gmail.com>
2026-03-18 19:24:16 +0000

    Update sudoers.d

--- Episode 2/sudoers.d
+++ Episode 2/sudoers.d
@@ -3,3 +3,6 @@ Create new file with:
   sudo nano <username>
 
 n8n ALL=(ALL) NOPASSWD: ALL
+
+
+{I very much recommend asking an AI for a specific command based on the perimssions you want Gerald to do! This gives full passwordless sudo. Even rm -rf in the root dir!!}
